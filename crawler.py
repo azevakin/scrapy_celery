8820912bd7774bbb02390e9ec08f198b7ae5c756
Андрей Зевакин <azevakin@sdvor.com>
2023-02-18 14:44:20 +0500

    save results to file

--- crawler.py
+++ crawler.py
@@ -5,7 +5,11 @@ from scrapy.crawler import CrawlerProcess
 
 
 def crawl_spider(spider: Type[Spider], urls: List[str]):
-    crawler = CrawlerProcess()
+    crawler = CrawlerProcess(settings={
+        "FEEDS": {
+            "items.jsonl": {"format": "jsonl"},
+        },
+    })
     crawler.crawl(spider, start_urls=urls)
     crawler.start()
     crawler.stop()
